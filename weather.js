bda9ec98f4b719b0ac4637e3ef7e9b6bea643e36
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-06-14 13:51:58 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -10,9 +10,12 @@ $httpClient.get(weaapi, function(error, response, data){
         var city = "所在城市： " + obj.city;
         var wea = "天气状况： " + obj.wea + "  当前" + obj.tem + "℃  " + obj.tem2 + "℃～" + obj.tem1 + "℃";
         var air = "当前风力： " + obj.win + obj.win_speed + "  风速" + obj.win_meter + "\n空气指数： " + obj.air + "  " + obj.air_level + "\n友情提示： " + obj.air_tips;
-        var alarm = "天气警报： " + obj.alarm_type + obj.alarm_level + "\n告警提示： " + obj.alarm_title + "  " + obj.alarm_content + "\n更新时间： " + obj.date + " "+ obj.update_time;
-        let wmation = [city,wea,air,alarm];
-        $notification.post(wmation[0], wmation[1], wmation[2]+wmation[3]);
+        var alarms = obj.alarm.map(function(alarm) {
+            return "天气警报： " + alarm.alarm_type + alarm.alarm_level + alarm.alarm_title;
+        }).join("\n");
+        var updateTime = "更新时间： " + obj.date + " "+ obj.update_time;
+        let wmation = [city, wea, air + "\n" + alarms + "\n" + updateTime];
+        $notification.post(wmation[0], wmation[1], wmation[2]);                
         $done();
     }
 }
