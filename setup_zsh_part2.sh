e95ef059a0e692c7f609efb46182f1e91d9e5c0c
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-09-09 20:28:22 +0800

    Create setup_zsh_part2.sh

---
+++ setup_zsh_part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 4. 安装zsh-autosuggestions和zsh-syntax-highlighting
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions已经安装."
+fi
+
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting已经安装."
+fi
+
+# 5. 修改.zshrc 文件
+sed -i '/^plugins=(/,/)/c\plugins=(\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n)' ~/.zshrc
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="ys"/' ~/.zshrc
+sed -i 's/^# DISABLE_MAGIC_FUNCTIONS="true"/DISABLE_MAGIC_FUNCTIONS="true"/' ~/.zshrc
+
+# 6. 提示用户
+echo "zsh 已经全部配置完成，请手动执行source ~/.zshrc并重新进入终端"
